fee427d1985be420f949dcb4c958b28a723a795e
roger <roger.hernandez@coderio.co>
2020-11-17 08:29:14 -0300

    ADD: kick off project. develop infra is OK

---
+++ compose/local/django/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z django-db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+
+echo "Making makemigartions at django"
+# python /usr/src/app/src/manage.py migrate
+
+echo "Making migrate at django"
+# python /usr/src/app/src/manage.py migrate
+
+echo "Making runserver at django"
+python /usr/src/app/src/manage.py runserver 0.0.0.0:8000
